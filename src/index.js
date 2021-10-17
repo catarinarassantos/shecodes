8c6bd8392eef1a2ce3b46a55c5ca042183d3bf17
catarinarassantos <catarinarassantos@gmail.com>
2021-10-18 00:38:25 +0100

    Corrected img src on JS

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ function showCityInfo(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wm/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
